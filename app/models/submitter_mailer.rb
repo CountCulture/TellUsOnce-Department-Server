8f9100c13a18f655c5b679a520186f31ffa228e2
Chris <chris@david-taggarts-macbook.local>
2010-03-20 17:20:11 +0000

    First go at reg office info in email

--- app/models/submitter_mailer.rb
+++ app/models/submitter_mailer.rb
@@ -1,11 +1,12 @@
 class SubmitterMailer < ActionMailer::Base
-  def acknowledgment(submitter_hash, notification_hash)
+  def acknowledgment(submitter_hash, notification_hash, reg_offices=nil)
     @recipients   = submitter_hash['email']
     @from         = 'notifier@dwp_test_foo.co.uk'
     @subject      = "Acknowledgment of notification of death"
     @sent_on      = Time.now
     @body[:notification] = notification_hash
     @body[:submitter] = submitter_hash
+    @body[:reg_offices] = reg_offices
     @headers      = {}
     
   end
