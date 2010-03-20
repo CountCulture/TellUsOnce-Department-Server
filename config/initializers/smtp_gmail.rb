a98e19062fcdb06c9bd20c43a91ff3462683657b
Chris <chris@david-taggarts-macbook.local>
2010-03-20 14:57:30 +0000

    Added basic acknowledgment mailer

---
+++ config/initializers/smtp_gmail.rb
@@ -0,0 +1,12 @@
+config_file = "#{RAILS_ROOT}/config/smtp_gmail.yml"
+raise "Sorry, you must have #{config_file}" unless File.exists?(config_file)
+
+require 'tlsmail' if RAILS_ENV=='development'
+
+config_options = YAML.load_file(config_file) 
+ActionMailer::Base.smtp_settings = {
+  :address => "smtp.gmail.com",
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}.merge(config_options) # Configuration options override default options
