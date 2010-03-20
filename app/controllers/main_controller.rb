ea7b63d20065e6efe362bbb188c88dff140e6db5
Chris <chris@david-taggarts-macbook.local>
2010-03-20 16:32:55 +0000

    Forgot to get controler to send email

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -6,6 +6,7 @@ class MainController < ApplicationController
     logger.error { "Params received =\n#{params}" }
     notification = Hash.from_xml(params[:notification])['notification']   
     @submitter, @change_notification = notification['submitter'], notification['change_notification']
+    SubmitterMailer.deliver_acknowledgment!(@submitter, @change_notification)
     render :text => "Successfully submitted details for #{@submitter['first_name']} #{@submitter['family_name']}", :status => :created
   rescue
     render :text => 'Error', :status => 422
